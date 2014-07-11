0c55d66ed4fa8d537d79e26316898075886541f2
Safia Abdalla <seabdalla@gmail.com>
2014-07-11 14:39:35 +0000

    Added functionality to store options

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-window.accurateInterval = function(time, fn) {
+window.accurateInterval = function (time, fn) {
     var cancel, nextAt, timeout, wrapper, _ref;
     nextAt = new Date().getTime() + time;
     timeout = null;
     if (typeof time === 'function') _ref = [time, fn], fn = _ref[0], time = _ref[1];
-    wrapper = function() {
+    wrapper = function () {
         nextAt += time;
         timeout = setTimeout(wrapper, nextAt - new Date().getTime());
         return fn();
     };
-    cancel = function() {
+    cancel = function () {
         return clearTimeout(timeout);
     };
     timeout = setTimeout(wrapper, nextAt - new Date().getTime());
@@ -17,52 +17,56 @@ window.accurateInterval = function(time, fn) {
     };
 };
 
-var formatTime = function(milliseconds) {
-    var pad = function(number) {
-        return (number < 10 ?  "0": "") + number; 
+var formatTime = function (milliseconds) {
+    var pad = function (number) {
+        return (number < 10 ? "0" : "") + number;
     };
     var notSecond = milliseconds % 1000;
     milliseconds = (milliseconds - notSecond) / 1000;
     var seconds = milliseconds % 60;
     milliseconds = (milliseconds - seconds) / 60;
-    var mins = milliseconds % 60; 
+    var mins = milliseconds % 60;
     return pad(mins) + ":" + pad(seconds);
 }
 
-var Pomodoro = function() {
+var minutesToMilliseconds = function (minutes) {
+    return minutes * 60 * 1000;
+}
+
+var Pomodoro = function () {
     var element = document.getElementById("countdown");
     var remaining, targetTime, pauseTime;
 
-    this.start = function(duration) {
+    this.start = function (duration) {
         targetTime = new Date().setTime(new Date().getTime() + duration)
-        workTimer = accurateInterval(function() {
+        workTimer = accurateInterval(function () {
             run(targetTime);
         }, 1000)
     }
 
-    var run = function(target) {
+    var run = function (target) {
         remaining = target - new Date;
         if (remaining <= 0) {
             workTimer.cancel();
         } else {
-            element.innerHTML = formatTime(remaining); 
+            element.innerHTML = formatTime(remaining);
         }
     }
 
-    this.pause = function() {
+    this.pause = function () {
         workTimer.cancel();
         pauseTime = new Date().getTime();
     }
 
-    this.resume = function() {
+    this.resume = function () {
         targetTime = targetTime + (new Date().getTime() - pauseTime);
-        workTimer = accurateInterval(function() {
+        workTimer = accurateInterval(function () {
             run(targetTime);
         }, 1000);
     }
 }
 
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener('DOMContentLoaded', function (event) {
     var timeOptions = {
         work: 1200000,
         short: 300000,
@@ -74,16 +78,16 @@ document.addEventListener('DOMContentLoaded', function(event) {
     var shortBreak = document.getElementById("short_break");
     var pause = document.getElementById("pause");
     var gear = document.getElementById("options_cog");
-    pomodoro.addEventListener('click', function(event) {
+    pomodoro.addEventListener('click', function (event) {
         timer.start(timeOptions.work);
     });
-    longBreak.addEventListener('click', function(event) {
+    longBreak.addEventListener('click', function (event) {
         timer.start(timeOptions.long);
     });
-    shortBreak.addEventListener('click', function(event) {
+    shortBreak.addEventListener('click', function (event) {
         timer.start(timeOptions.short);
     });
-    pause.addEventListener('click', function(event) {
+    pause.addEventListener('click', function (event) {
         if (pause.innerText == "Pause") {
             timer.pause();
             pause.innerHTML = "Resume";
@@ -92,7 +96,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
             pause.innerHTML = "Pause";
         }
     });
-    gear.addEventListener('click', function(event) {
+    gear.addEventListener('click', function (event) {
         var options = document.getElementById("options");
         var style = window.getComputedStyle(options);
         var display = style.getPropertyValue("display");
@@ -100,6 +104,13 @@ document.addEventListener('DOMContentLoaded', function(event) {
             options.style.display = "block";
         } else {
             options.style.display = "none";
+            timeOptions.work = minutesToMilliseconds(document.getElementById(
+                "pomodoro_length").value);
+            timeOptions.short = minutesToMilliseconds(document.getElementById(
+                "short_break_length").value);
+            timeOptions.long = minutesToMilliseconds(document.getElementById(
+                "long_break_length").value);
+            console.log(timeOptions);
         }
     });
 });
